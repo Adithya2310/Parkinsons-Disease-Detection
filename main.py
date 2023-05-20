8ff922f992f7b5d941c7bcf6eda557f044ee1150
Adithya2310 <Adithyanandi099@gmail.com>
2023-05-20 14:06:39 +0530

    skeletal web application wto detect using spiral model

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask,render_template
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
+from wtforms.validators import InputRequired
+from predict_parkinsons import predict_image
 import os
 
 
@@ -10,8 +12,12 @@ app.config['SECRET_KEY']='mysecretkey'
 app.config['UPLOAD_FOLDER']='static/images'
 
 
+waveModel = os.path.join("models/", "random_forest_wave_model.pkl")
+spiralModel = os.path.join("models/", "random_forest_spiral_model.pkl")
+
+
 class UploadFileForm(FlaskForm):
-    file=FileField("File")
+    file=FileField("File",validators=[InputRequired()])
     submit=SubmitField("Upload File")
 
 @app.route('/',methods=["GET","POST"])
@@ -20,9 +26,16 @@ def home():
     form = UploadFileForm()
     if form.validate_on_submit():
         file=form.file.data
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
-        return "File has been uploaded"
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename('PatientImage.png')))
+        pred=predict_image('static/images/PatientImage.png', spiralModel)
+        print("the output from the model is", pred)
+        if pred == "Healthy":
+            return "The patient is healthy"
+        else:
+            return "The patient is affected with parkinsons"
     return render_template('index.html',form=form)
+
+
 
 if __name__=='__main__':
     app.run(debug=True)
