329c7e2487e825e5e1d0fe7da4e93b25872b244f
Adithya2310 <Adithyanandi099@gmail.com>
2023-06-27 14:15:51 +0530

    final commit

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from werkzeug.utils import secure_filename
 from wtforms.validators import InputRequired
 from predict_parkinsons import predict_image
 import os
+import base64
+from PIL import Image
+import io
+
 
 
 app=Flask(__name__)
@@ -12,12 +16,16 @@ app.config['UPLOAD_FOLDER']='static/images'
 
 
 waveModel = os.path.join("models/", "random_forest_wave_model.pkl")
-spiralModel = os.path.join("models/", "random_forest_spiral_model.pkl")
+spiralModel = os.path.join("models/", "random_forest_wave_model.pkl")
 
 # route of index.html
 @app.route("/",methods=["GET","POST"])
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
+
+@app.route("/upload",methods=["GET","POST"])
+def upload():
+    return render_template("upload.html")
 
 # to get the about route
 @app.route("/about",methods=["GET","POST"])
@@ -63,7 +71,72 @@ def uploadWave():
         else:
             return render_template('uploadWave.html',result=True)
     return render_template('uploadWave.html',result="null")
-        
+
+# code to detect the drawn images
+@app.route("/drawSpiral",methods=["GET","POST"])
+def drawSpiral():
+    return render_template('padspiral.html',result="null")
+
+@app.route("/drawWave",methods=["GET","POST"])
+def drawWave():
+    return render_template('padwave.html',result="null")
+
+@app.route('/predictSpiral',methods=["POST"])
+def predictSpiral():
+    # Get the submitted image data (data_url) from the form
+    data_url = request.form['url']
+
+    # Extract the base64-encoded image data from the data_url
+    image_data = data_url.split(',')[1]
+
+    # Decode the base64 image data
+    img_bytes = base64.b64decode(image_data)
+
+    # Create a PIL image object from the binary image data
+    img = Image.open(io.BytesIO(img_bytes))
+
+    # Create a new image with white background
+    new_img = Image.new("RGB", img.size, (255, 255, 255))
+    new_img.paste(img, (0, 0), img)
+
+    # Save the image file to the upload folder
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename('DrawnImage.png'))
+    new_img.save(file_path)
+    pred=predict_image('static/images/DrawnImage.png', spiralModel)
+    if pred == "Healthy":
+        return render_template('padspiral.html',result=False)
+    else:
+        return render_template('padspiral.html',result=True)
+
+
+@app.route('/predictWave',methods=["POST"])
+def predictWave():
+    # Get the submitted image data (data_url) from the form
+    data_url = request.form['url']
+
+    # Extract the base64-encoded image data from the data_url
+    image_data = data_url.split(',')[1]
+
+    # Decode the base64 image data
+    img_bytes = base64.b64decode(image_data)
+
+    # Create a PIL image object from the binary image data
+    img = Image.open(io.BytesIO(img_bytes))
+
+    # Create a new image with white background
+    new_img = Image.new("RGB", img.size, (255, 255, 255))
+    new_img.paste(img, (0, 0), img)
+
+    # Save the image file to the upload folder
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename('DrawnImage.png'))
+    new_img.save(file_path)
+
+    pred=predict_image('static/images/DrawnImage.png', waveModel)
+    if pred == "Healthy":
+        return render_template('padwave.html',result=False)
+    else:
+        return render_template('padwave.html',result=True)
+
 
     # form = UploadFileForm()
     # if form.validate_on_submit():
